c1da5d759054ae181317f1d6bd7fac220ddee5c1
Storm Galloway <storm.galloway@variq.com>
2020-07-29 08:47:32 -0400

    added dynamic post creation

--- mean-stack-practice/src/app/posts/post-create/post-create.component.ts
+++ mean-stack-practice/src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+
+
+import { Post } from '../post.model'
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 
 })
 export class PostCreateComponent{
-  enteredValue = '';
-  newPost = 'No Content';
+  enteredContent = '';
+  enteredTitle = '';
+  @Output()
+  postCreated = new EventEmitter<Post>();
 
   onAddPost(){
-    this.newPost = this.enteredValue;
+    const post: Post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
   }
 }
