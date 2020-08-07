0c727c4b9e72ee361f8dc2847d3c0259f356111e
Storm Galloway <storm.galloway@variq.com>
2020-08-07 08:42:37 -0400

    added delete post functionality

--- mean-stack-practice/src/app/posts/post-list/post-list.component.ts
+++ mean-stack-practice/src/app/posts/post-list/post-list.component.ts
@@ -26,6 +26,18 @@ export class PostListComponent implements OnInit, OnDestroy{
       this.posts = posts;
     });
   }
+  //delets posts based off id
+  onDelete(postId: string){
+    var confirmDelete = confirm("Are you sure you want to delete this post? This cannot be undone.");
+    if (confirmDelete == true){
+      var postDeleted = this.postsService.deletePost(postId);
+      if (postDeleted == true) {
+        alert("Post has been deleted!");
+      }
+    } else {
+      return;
+    }
+  }
 
   ngOnDestroy(){
     this.postsSub.unsubscribe;
--- mean-stack-practice/src/app/posts/post.service.ts
+++ mean-stack-practice/src/app/posts/post.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators';
 
 import { Post } from './post.model';
 
@@ -12,9 +13,21 @@ export class PostsService{
   constructor(private http: HttpClient) {};
 
   getPosts(){
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-    .subscribe( (postData) => {
-      this.posts = postData.posts;
+    this.http.get<{message: string, posts: any }>('http://localhost:3000/api/posts')
+    //to change id to _id
+    .pipe(map((postData => {
+      //replace every post with...
+      return postData.posts.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      });
+    })))
+    //subscribe to observable with remapped posts
+    .subscribe( (transformedPosts) => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
     });
   }
@@ -23,14 +36,35 @@ export class PostsService{
     return this.postsUpdated.asObservable();
   }
 
+    //adds post
   addPosts(title: string, content: string){
     const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+    this.http
+    .post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
     .subscribe( (responseData) => {
-      console.log(responseData.message);
+      //store postId from the result of saving to database
+      const id = responseData.postId;
+      //update const post above with id
+      post.id = id;
+      //push post to posts array
       this.posts.push(post);
+      //informs whole app of update
       this.postsUpdated.next([...this.posts]);
     });
+  }
 
+  //deletes post
+  deletePost(postId: string){
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe( () => {
+        //update post array on frontend by keeping ones where it is not equal and deleteing if it is equal
+        const updatedPosts = this.posts.filter(post => post.id !== postId);
+        //makes the array posts equal to what has been filtered
+        this.posts = updatedPosts;
+        //informs whole app of update
+        this.postsUpdated.next([...this.posts]);
+      });
+      //return boolean to confirm
+      return true;
   }
 }
