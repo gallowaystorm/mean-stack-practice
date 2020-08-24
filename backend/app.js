0359041c513190000dc6f31804442c3ea38ee84b
Storm Galloway <storm.galloway@variq.com>
2020-08-24 16:23:40 -0400

    need to add next() to the check-auth.js

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
     );
     res.setHeader(
       "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS, PUT"
+      "OPTIONS, GET, POST, PATCH, DELETE, PUT"
     );
     next();
   });
--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -8,8 +8,10 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization
         //assign value to header and split on white space and the token is the 1st index because 'bearer' will be the first part
         .split(' ')[1];
+        console.log(token);
         //verify token with same secret used to make it (will throw error if token is not verified or not correct)
         jwt.verify(token, 'secret_this_should_be_longer');
+        next();
     } catch (error) {
         res.status(401).json({
             message: "Auth failed!"
