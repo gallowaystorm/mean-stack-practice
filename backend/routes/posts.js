568104955c470b1b268f46eed33cd893918846d9
Storm Galloway <storm.galloway@variq.com>
2020-08-28 07:38:34 -0400

    seperated multer middleware

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,48 +1,20 @@
 const express = require('express');
-const multer = require('multer');
 
 const router = express.Router();
 
 const checkAuth = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 const PostsController = require('../controller/posts');
 
-const MIME_TYPE_MAP = {
-    'image/png': 'png',
-    'image/jpeg': 'jpg',
-    'image/jpg': 'jpg'
-};
-
-//for multer to know where to put files 
-const storage = multer.diskStorage( {
-    //cb = callback
-    destination: (req, file, cb) => {
-        //check if mimetype is valid on server side
-        const isValid = MIME_TYPE_MAP[file.mimetype];
-        let error = new Error('Invalid mime type');
-        if (isValid) { 
-            error = null;
-        }
-        //images will be stored in backend/images
-        cb(error, "../backend/images");
-    },
-    //tell multer what the file name will be 
-    filename: (req, file, cb) => {
-        //change name to lowercase and then replace white space with -
-        const name = file.originalname.toLowerCase().split(' ').join('-');
-        const extension = MIME_TYPE_MAP[file.mimetype];
-        //create the name of the file with extention
-        cb(null, name + '-' + Date.now() + '.' + extension);
-    }
-});
 
 //create posts
     //multer will store in storage and expects a single file with the propery "image"
     // checkAuth is to verify token
-router.post('', checkAuth, multer({storage: storage}).single('image'), PostsController.createPost);
+router.post('', checkAuth, extractFile, PostsController.createPost);
 
 //update post
 //middleware for images
-router.put('/:id', checkAuth, multer({storage: storage}).single('image'), PostsController.updatePost);
+router.put('/:id', checkAuth, extractFile, PostsController.updatePost);
 
 
 //get all posts
